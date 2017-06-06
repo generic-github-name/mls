9ed3bd954827dc785dd1219737bcf0363da82112
DMP <NoEmail>
2017-06-05 21:00:42 -0700

    Add broken attempt to scrape injury data

--- webscrape/scrape_injuries.r
+++ webscrape/scrape_injuries.r
@@ -23,33 +23,43 @@ reScrape = FALSE
 # --------------------------------------------------
 
 
-# ----------------------------------------------------------------------------------------------------
-# loop over years and scrape every backup known to the wayback machine
-i=1
-for(y in 2013:2017) {
-	# store html info
-	url = paste0('https://web.archive.org/web/', y, '0515000000*/https://www.mlssoccer.com/injuries')
-	proxy.string = use_proxy("")
-	htmlpage = html_session(url, proxy.string)
-		
-	# scrape data on every archive they have crawled
-	if (i==1) archives = as.character(htmlpage %>% html_nodes('#calOver a'))
-	if (i>1) archives = c(archives, as.character(htmlpage %>% html_nodes('#calOver a')))
-	i=i+1
+# # ----------------------------------------------------------------------------------------------------
+# # loop over years and scrape every backup known to the wayback machine
+
+# # SOMEHOW THIS IS BROKEN SINCE I FIRST WROTE IT!!!
+
+# i=1
+# for(y in 2013:2017) {
+	# # store html info
+	# url = paste0('https://web.archive.org/web/', y, '0515000000*/https://www.mlssoccer.com/injuries')
+	# proxy.string = use_proxy("")
+	# htmlpage = html_session(url, proxy.string)
 	
-	# sleep because the wayback machine seems to know what I'm up to
-	Sys.sleep(5)
-}
-# ----------------------------------------------------------------------------------------------------
+	# # scrape data on every archive they have crawled
+	# if (i==1) archives = as.character(htmlpage %>% html_nodes('#calOver a'))
+	# if (i>1) archives = c(archives, as.character(htmlpage %>% html_nodes('#calOver a')))
+	# i=i+1
+	
+	# # sleep because the wayback machine seems to know what I'm up to
+	# Sys.sleep(5)
+# }
+# # ----------------------------------------------------------------------------------------------------
 
 
 # -----------------------------------------------------------------------------------------
 # Loop over backups and get the data from them
+
+# NOTE: prior to March 6th, 2015, MLS used a different table format and seemed ot update less consistently
+# for now, I'll just focus on 2015+
+archives = fread('./webscrape/injuries/list_of_archives.csv')
+archives = gsub('.rds', '', archives[[1]])
+
 i = 1
 for(a in archives) {
 
-	# store URL ID from wayback and skip if already scraped
-	id = str_split(a, '/')[[1]][3]
+	# # store URL ID from wayback and skip if already scraped
+	# id = str_split(a, '/')[[1]][3]
+	id = a
 	outFile = paste0(outDir, id, '.rds')
 	if (!reScrape & file.exists(outFile)) next 
 
@@ -59,7 +69,7 @@ for(a in archives) {
 	htmlpage = html_session(url, proxy.string)
 	
 	# scrape injury data
-	injury_scr = htmlpage %>% html_nodes('#node-74493 td , td p')
+	injury_scr = htmlpage %>% html_nodes('p')
 	injury_scr = as.character(injury_scr)
 	
 	# store current injury list in its raw state
@@ -76,24 +86,40 @@ for(a in archives) {
 
 # -----------------------------------------------------------------------------------------
 # Append injury data
-files = list.files(outDir)
-for(f in files) {
-	tmp = data.table(readRDS(paste0(outDir, '/', f)))
-	tmp[, V1:=gsub('\t', '', V1)]
-	tmp[, V1:=gsub('\n', '', V1)]
-	tmp[, V1:=gsub('</li>','',V1)]
-	tmp[, V1:=gsub('<li>',',',V1)]
-	tmp[, V1:=gsub('</ul>','',V1)]
-	tmp[, V1:=gsub('<ul>','',V1)]
-	tmp[, V1:=gsub('<td>','',V1)]
-	tmp[, V1:=gsub('</td>','',V1)]
-	tmp[, V1:=gsub('<p>','',V1)]
-	tmp[, V1:=gsub('</p>','',V1)]
-	tmp[, V1:=gsub('</i>','',V1)]
-	tmp[, V1:=gsub('<td style="vertical-align: top;">,', '', V1)]
-	tmp[, V1:=gsub('<i style="text-align: right;">', ',', V1)]
-	tmp[, V1:=gsub('<td style="vertical-align: top;">', ',', V1)]
+
+# so broken. the mls injury website seems to switch html format every few months. this makes it impossible to munge the data
+
+i=1
+for(f in archives) {
+	tmp = data.table(readRDS(paste0(outDir, '/', f, '.rds')))
+	tmp[, V1:=gsub('<p style="margin: 0px 0px 8px;">', '', V1)]
+	tmp[, V1:=gsub('<strong>', '', V1)]
+	tmp[, V1:=gsub('</strong>', '', V1)]
+	tmp[, V1:=sub('<a.*</a>', '', V1)] # this removes urls
+	tmp[, V1:=sub('</p>', '', V1)]
+	tmp[, V1:=sub(' -', '', V1)]
+	tmp[, V1:=sub(' -', '', V1)]
+	tmp[, V1:=sub('<b>', '', V1)]
+	tmp[, V1:=sub('</b>', '', V1)]
+	tmp[, V1:=sub('<p style="margin: 0px 0px 8px;">', '', V1)]
+	tmp[, V1:=sub('<p>', '', V1)]
+	tmp[, V1:=sub('\n', '', V1)]
+	tmp[, V1:=sub('<p style="font-size:13px;">', '', V1)]
 	
-	tmp[grepl('/files/', V1), team:=V1
+	tmp = tmp[!grepl('copyrights', V1)]
+	tmp = tmp[!grepl('Last Updated', V1)]
+	tmp = tmp[!grepl('NOTE:', V1)]
+	tmp = tmp[V1!='']
+	tmp = tmp[V1!='None']
+	
+	tmp[grepl('OUT',V1), status:='Out']
+	tmp[grepl('QUESTIONABLE',V1), status:='Questionable']
+	tmp[is.na(status), status:='Unspecified']
+
+	tmp[, date:=as.Date(str_sub(f, 1, 8),format='%Y%m%d')]
+	
+	if (i==1) data = tmp
+	if (i>1) data = cbind(data, tmp)
+	i=i+1
 }
 # -----------------------------------------------------------------------------------------
