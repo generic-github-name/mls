d80dd9513fc8a0e5705160cd6bb823560a15da7a
DMP <NoEmail>
2017-06-21 08:17:59 -0700

    Finish spatial analysis

--- defensive_actions/defensive_spatial_analysis.r
+++ defensive_actions/defensive_spatial_analysis.r
@@ -21,21 +21,27 @@ library(RColorBrewer)
 # ------------------
 
 
-# ------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # Files, directories and settings
 
+# analytical resolution (pixel size in the models)
+r = 1
+
+# prediction resolution (pixel size in the graphs)
+r2 = 1
+
+# year of analysis
+year = 2016
+
 # input file
-inFile = './webscrape/ASA/raw defensive actions_2016.csv'
+inFile = paste0('./webscrape/ASA/', year, '/raw defensive actions.csv')
 
 # shapefile of field
 outLines = './_common/Soccer_Fields/soccer_field.jpg'
 
-# analytical resolution (pixel size)
-r = 1
-
 # output file
-outFile = paste0('./defensive_actions/defensive_actions_',r,'.pdf')
-# ------------------------------------------------------------------
+outFile = paste0('./defensive_actions/defensive_actions_',r,'_',year,'.pdf')
+# --------------------------------------------------------------------------
 
 
 # ---------------------------------------------------------------------------
@@ -48,9 +54,14 @@ data = fread(inFile)
 data[, team:=factor(team, levels=unique(data$team)[order(unique(data$team))], order=TRUE)]
 
 # briefly visualize the data
-ggplot(data[team=='Seattle'], aes(y=y,x=x,shape=action)) + 
+ggplot(data[team=='Toronto'], aes(y=y,x=x,shape=action)) + 
 	geom_point() + 
 	facet_wrap(~player)
+
+# briefly visualize the data
+ggplot(data, aes(y=y,x=x,shape=action)) + 
+	geom_point() + 
+	facet_wrap(~team)
 	
 # extra term for ease of use
 data[, xy:=x*y]	
@@ -82,13 +93,16 @@ gridData[, y3:=y^3]
 # Analyze
 
 # simple negative binomial
+print('Running model...')
 nbFit = glm.nb(actions_pg ~ team*y*x*y2*x2*x3, gridData)
 
 # simple glm of success probability (takes about 7 minutes with x3, 1 minute with only x2 and y2)
+print('Running probability of successful defensive action model...')
 glmFit = glm(outcome ~ team*y*x*y2*x2, data, family='binomial')
 
 # predict for every team
-cfData = data.table(expand.grid(seq(0,100, by=r), seq(0,100, by=r), unique(data$team)))
+print('Predicting surfaces...')
+cfData = data.table(expand.grid(seq(0,100, by=r2), seq(0,100, by=r2), unique(data$team)))
 setnames(cfData, c('x','y','team'))
 cfData[, x2:=x^2]
 cfData[, y2:=y^2]
@@ -98,12 +112,12 @@ cfData[, pred:=inv.logit(predict(glmFit, newdata=cfData))]
 cfData[, pred_nb:=exp(predict(nbFit, newdata=cfData))]
 
 # cap estimates to avoid long tails
-q5 = quantile(cfData$pred, p=.1)
-q95 = quantile(cfData$pred, p=.9)
+q5 = quantile(cfData$pred, p=.05)
+q95 = quantile(cfData$pred, p=.95)
 cfData[pred<q5, pred:=q5]
 cfData[pred>q95, pred:=q95]
-q5 = quantile(cfData$pred_nb, p=.1)
-q95 = quantile(cfData$pred_nb, p=.9)
+q5 = quantile(cfData$pred_nb, p=.05)
+q95 = quantile(cfData$pred_nb, p=.95)
 cfData[pred_nb<q5, pred_nb:=q5]
 cfData[pred_nb>q95, pred_nb:=q95]
 # ---------------------------------------------------------------------------
@@ -127,15 +141,35 @@ lines$raster = as.raster(as.matrix(colors))
 # Graph analysis
 
 # colors
-colors = brewer.pal(8, 'RdYlBu')
+colors = rev(brewer.pal(8, 'RdYlBu'))
 
-# factor to make expected actions integer-scale numbers
-f = 34
+
+# location/scale parameters for the field lines
+if (year==2016) {
+	x=-10
+	y=-9
+	width=120
+	height=118
+	pdfw = 10
+	pdfh = 6
+	# factor to make expected actions integer-scale numbers
+	f = 15
+}
+if (year==2017) {
+	x=-10
+	y=-9
+	width=120
+	height=118
+	pdfw = 7.9
+	pdfh = 6
+	# factor to make expected actions integer-scale numbers
+	f = 15
+}
 
 # make heatmaps
-p1 = ggplot(cfData[team=='Toronto'], aes(y=y,x=x,fill=pred_nb*f)) + 
+p1 = ggplot(cfData, aes(y=y,x=x,fill=pred_nb*f)) + 
 	geom_tile() + 
-	draw_grob(lines, x=-10, y=-9, width=120, height=118) + # this is bad. I have to match the location/scale to the aspect ratio of the output
+	draw_grob(lines, x=x, y=y, width=width, height=height) + # this is bad. I have to match the location/scale to the aspect ratio of the output
 	facet_wrap(~team) + 	
 	scale_fill_gradientn(paste('Actions\n Per',f,'\nGames'), colors=colors) +
 	labs(title='Expected Number of Defensive Actions', y='Defensive End') + 
@@ -146,7 +180,7 @@ p1 = ggplot(cfData[team=='Toronto'], aes(y=y,x=x,fill=pred_nb*f)) +
 # make heatmaps
 p2 = ggplot(cfData, aes(y=y,x=x,fill=pred)) + 
 	geom_tile() + 
-	draw_grob(lines, x=-10, y=-9, width=120, height=118) + # this is bad. I have to match the location/scale to the aspect ratio of the output
+	draw_grob(lines, x=x, y=y, width=width, height=height) + # this is bad. I have to match the location/scale to the aspect ratio of the output
 	facet_wrap(~team) + 	
 	scale_fill_gradientn('Probability', colors=colors) +
 	labs(title='Probability of Successful Defensive Action', y='Defensive End') + 
@@ -173,12 +207,25 @@ p4 = ggplot(aggregates[, sum(actions),by=c('y','team')], aes(y=V1, x=y)) +
 	theme_bw() + 
 	theme(plot.title=element_text(hjust=.5, size=16), axis.title.y=element_text(size=14), 
 	axis.title.x=element_text(size=14), axis.text.x=element_text(angle=315, hjust=0, size=9)) 
+
+# make a surface of the data
+p5 = ggplot(gridData, aes(y=y,x=x,fill=actions_pg*f)) + 
+	geom_tile() + 
+	draw_grob(lines, x=x, y=y, width=width, height=height) + # this is bad. I have to match the location/scale to the aspect ratio of the output
+	facet_wrap(~team) + 	
+	scale_fill_gradientn(paste('Actions\n Per',f,'\nGames'), colors=colors) +
+	labs(title='Expected Number of Defensive Actions', y='Defensive End') + 
+	theme_minimal()	+ 
+	theme(axis.title.x=element_blank(), axis.title.y=element_text(size=14), axis.text=element_blank(), 
+	axis.ticks=element_blank(), plot.title=element_text(hjust=.5, size=16), line = element_blank())
+
 	
 # open pdf
-pdf(outFile, height=6, width=10)
+pdf(outFile, height=pdfh, width=pdfw)
 p1
 p2
 p3
 p4
+p5
 dev.off()
 # ---------------------------------------------------------------------------
