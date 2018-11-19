b81ec0c0ec08480bf49f1c2012f96465eccd8dc1
generic-github-name <rise.above.flatland@gmail.com>
2018-11-18 19:06:13 -0800

    Remove files

--- webscrape/ASA/2018/scrapers/name_substitution.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i -- 's/Kim Kee-hee/Kim Kee-Hee/g' Opta\ JSON\ Data/*
-sed -i -- 's/Franck Pangop/Frantz Pangop/g' Opta\ JSON\ Data/*
-sed -i -- 's/Yefferson Quintana/Yeferson Quintana/g' Opta\ JSON\ Data/*
-sed -i -- 's/Sebastian Ibeagha/Sebastien Ibeagha/g' Opta\ JSON\ Data/*
-sed -i -- 's/Gregory Van der Wiel/Gregory van der Wiel/g' Opta\ JSON\ Data/*
-
